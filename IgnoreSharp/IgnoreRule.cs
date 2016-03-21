d2c6b7799a3480c745ead16063e54c3f3cd7519e
Mark Bell <me@markb.co.uk>
2016-03-21 13:58:32 +0000

    Rework match method to properly apply .gitignore spec
    https://git-scm.com/docs/gitignore

--- IgnoreSharp/IgnoreRule.cs
+++ IgnoreSharp/IgnoreRule.cs
@@ -18,7 +18,7 @@ namespace IgnoreSharp
             Pattern = pattern;
             Exclude = isExclude;
             DirectoryOnly = pattern.EndsWith("/", StringComparison.OrdinalIgnoreCase);
-            RegularExpression = GlobPatternToRegex(isExclude ? pattern : pattern.TrimStart('!'));
+            RegularExpression = GlobPatternToRegex(isExclude ? pattern : pattern.TrimStart('!'), DirectoryOnly);
         }
 
         public bool IsMatch(string s)
@@ -28,23 +28,44 @@ namespace IgnoreSharp
 
         // Modified from the Glob class in the Unity Application Block
         // https://github.com/unitycontainer/unity/blob/4fc7c789aecf4415db2688753dabe18421296222/source/Unity.Interception/Src/Utilities/Glob.cs
-        private Regex GlobPatternToRegex(string pattern)
+        private Regex GlobPatternToRegex(string pattern, bool directoryOnly)
         {
-            StringBuilder regexPattern = new StringBuilder(pattern);
-
             string[] globLiterals = { "\\", ".", "$", "^", "{", "(", "|", ")", "+" };
 
             foreach (string globLiteral in globLiterals)
             {
-                regexPattern.Replace(globLiteral, @"\" + globLiteral);
+                pattern.Replace(globLiteral, @"\" + globLiteral);
             }
 
-            regexPattern.Replace("*", ".*");
-            regexPattern.Replace("?", ".");
-            
+            if (!pattern.Contains("/"))
+            {
+                // If there are no path separators in the rule, treat asterisks the same way as double asterisks
+                pattern = Regex.Replace(pattern, @"\*+", ".*");
+            }
+            else
+            { 
+                // Two consecutive asterisks should be equivalent to .*
+                // Replace any occurrences with a placeholder, so that we can match single asterisks differently with the next pass
+                pattern = Regex.Replace(pattern, @"\*\*", ".{@}");
+                // Now replace single asterisks - should match everything except forward slashes
+                pattern = Regex.Replace(pattern, @"\*", "[^/]+");
+                // Now single asterisks have been dealt with, replace any double-asterisk placeholders with the correct regex pattern
+                pattern = Regex.Replace(pattern, @"\.\{\@\}", ".*");
+            }
+
+            pattern = Regex.Replace(pattern, @"\?", ".");
+
+            // Match start of line
+            pattern = "^" + pattern;
+
+            // If this is a directory ignore rule, we want to match anything which *starts* with the rule as well
+            // as literal matches, so only add the end of line token if this is a file rule
+            if(!directoryOnly)
+                pattern = pattern + "$";
+
             // TODO: Is this supposed to be case-sensitive? 
             // Glob on Unix is case-sensitive, but running tests on Windows it seems that the casing of .gitignore rules makes no difference...
-            return new Regex(regexPattern.ToString(), RegexOptions.IgnoreCase);
+            return new Regex(pattern, RegexOptions.IgnoreCase);
         }
     }
 }
