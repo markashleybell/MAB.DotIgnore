c51ab40282812d8b2fcca806fa557dd91f388012
Mark Bell <me@markb.co.uk>
2016-05-17 07:00:46 +0100

    Add gulp scaffolding

---
+++ IgnoreSharp.Tests/gulpfile.js
@@ -0,0 +1,11 @@
+﻿var gulp = require('gulp'),
+    exec = require('child_process').exec;
+
+gulp.task('task', function (callback) {
+    // TODO: Run OpenCover and ReportGenerator here
+    exec('ping localhost', function (err, stdout, stderr) {
+        //console.log(stdout);
+        //console.log(stderr);
+        callback(err);
+    });
+});
