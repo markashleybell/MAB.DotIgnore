616a4fe401ab19fae3edc05521efe7f637ccb44e
Mark Bell <me@markb.co.uk>
2016-05-27 07:34:27 +0100

    Tidy up using statements

--- MAB.DotIgnore.Test/IgnoreListTests.cs
+++ MAB.DotIgnore.Test/IgnoreListTests.cs
@@ -1,9 +1,6 @@
 ﻿using NUnit.Framework;
-using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
 
 namespace MAB.DotIgnore.Tests
 {
--- MAB.DotIgnore.Test/IgnoreRuleTests.cs
+++ MAB.DotIgnore.Test/IgnoreRuleTests.cs
@@ -1,8 +1,5 @@
 ﻿using NUnit.Framework;
 using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Linq;
 using System.IO;
 
 namespace MAB.DotIgnore.Tests
