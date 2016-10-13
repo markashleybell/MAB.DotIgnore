b162391d4996cfdd6648ae6355bd0c9f119cd74a
Mark Bell <me@markb.co.uk>
2016-10-13 07:13:11 +0100

    Add output comparison test to check wildmatch output against C reference code

--- MAB.DotIgnore.Test/IntegrationTests.cs
+++ MAB.DotIgnore.Test/IntegrationTests.cs
@@ -1,9 +1,18 @@
 ﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 
 namespace MAB.DotIgnore.Tests
 {
+    internal class Test
+    {
+        public string Text { get; set; }
+        public string Pattern { get; set; }
+    }
+
     [TestFixture]
     public class IntegrationTests
     {
@@ -19,6 +28,63 @@ namespace MAB.DotIgnore.Tests
         public void SetUp()
         {
 
+        }
+
+        [Test]
+        public void Compare_Wildmatch_Output_With_C_Function()
+        {
+            var tests = File.ReadAllLines(_basePath + @"\tests.txt")
+                    .Where(l => !l.StartsWith("#") && !string.IsNullOrWhiteSpace(l))
+                    .Select(l => l.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries))
+                    .Select(l => new Test { Text = l[2], Pattern = l[3] })
+                    .ToList();
+
+            tests.ForEach(t => {
+                Console.WriteLine(t.Pattern);
+                var referenceResult = ReferenceMatchPattern(t.Pattern, t.Text, true);
+                var testResult = WildMatch.IsMatch(t.Pattern, t.Text, MatchFlags.CASEFOLD);
+                Assert.AreEqual(referenceResult, testResult);
+            });
+        }
+
+        private Process CreateProcess(string executableFilename, string arguments, string workingDirectory)
+        {
+            return new Process {
+                EnableRaisingEvents = true,
+                StartInfo = new ProcessStartInfo {
+                    FileName = executableFilename,
+                    Arguments = arguments,
+                    WorkingDirectory = workingDirectory,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                }
+            };
+        }
+
+        private int ReferenceMatchPattern(string pattern, string text, bool caseFold)
+        {
+            var workingDirectory = _basePath + @"\git-wildmatch";
+    
+            var log = new List<string>();
+    
+            var arguments = pattern + " " + text + " " + (caseFold ? 1 : 0);
+    
+            using (var build = CreateProcess(workingDirectory + @"\wm.exe", arguments, workingDirectory))
+            {
+                build.Start();
+
+                build.OutputDataReceived += (sender, e) => log.Add("0> " + e.Data);
+                build.BeginOutputReadLine();
+
+                build.ErrorDataReceived += (sender, e) => log.Add("1> " + e.Data);
+                build.BeginErrorReadLine();
+
+                build.WaitForExit();
+        
+                return build.ExitCode;
+            }
         }
         
         [Test]
