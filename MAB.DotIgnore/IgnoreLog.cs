1eca747afe1f3fe48b94b68582e57b73b8b6d985
Mark Bell <me@markb.co.uk>
2017-01-30 12:33:34 +0000

    Add missing XML comments

--- MAB.DotIgnore/IgnoreLog.cs
+++ MAB.DotIgnore/IgnoreLog.cs
@@ -4,8 +4,15 @@ using System.Linq;
 
 namespace MAB.DotIgnore
 {
+    /// <summary>
+    /// Keeps track of which rules matched which path (including overrides etc).
+    /// </summary>
     public class IgnoreLog : Dictionary<string, List<string>>
     {
+        /// <summary>
+        /// Returns a "pretty printed" string representation of the rule match log.
+        /// </summary>
+        /// <returns>String representation of the match log.</returns>
         public override string ToString()
         {
             var nl = Environment.NewLine;
