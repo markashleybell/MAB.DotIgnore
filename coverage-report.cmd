8732d01a8226fe8592a9aae9ec0e169272cbaa04
Mark Bell <me@markb.co.uk>
2018-05-16 10:37:08 +0100

    Coverage report script is now just a batch file
    Gulp was unnecessary for this, and OpenCover will only run on Windows anyway.

---
+++ coverage-report.cmd
@@ -0,0 +1,17 @@
+@ECHO OFF
+
+ECHO Report generation takes approximately 3 minutes to run...
+
+"%UserProfile%\.nuget\packages\opencover\4.6.519\tools\OpenCover.Console.exe" ^
+-target:"C:\Program Files\dotnet\dotnet.exe" ^
+-targetargs:"test -f netcoreapp2.0 -c Debug MAB.DotIgnore.Test/MAB.DotIgnore.Test.csproj" ^
+-excludebyattribute:*.ExcludeFromTestCoverageAttribute ^
+-filter:"+[MAB.DotIgnore*]* -[MAB.DotIgnore.Test*]* -[MAB.DotIgnore]MAB.DotIgnore.ExcludeFromTestCoverageAttribute" ^
+-register:user ^
+-oldStyle ^
+-output:"reports\results.xml" ^
+-searchdirs:"MAB.DotIgnore.Test\bin\Debug\netcoreapp2.0"
+
+"%UserProfile%\.nuget\packages\reportgenerator\3.1.2\tools\ReportGenerator.exe" ^
+-reports:"reports\results.xml" ^
+-targetdir:"reports"
