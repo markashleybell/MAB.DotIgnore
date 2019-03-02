3739e5ed7dcf71c1de289b583a500235dc4bfe31
Mark Bell <me@markb.co.uk>
2019-03-02 21:17:41 +0000

    Test packages are all now at latest versions

--- tools/coverage.ps1
+++ tools/coverage.ps1
@@ -7,7 +7,7 @@ $ReportFolderPath = "..\$ReportFolderName"
 
 New-Item -Path ..\ -Type Directory -Name $ReportFolderName -Force | Out-Null
 
-&"$NuGetPackages\opencover\4.6.519\tools\OpenCover.Console.exe" `
+&"$NuGetPackages\opencover\4.7.922\tools\OpenCover.Console.exe" `
     -target:"C:\Program Files\dotnet\dotnet.exe" `
     -targetargs:"test -f netcoreapp2.0 -c Debug $TestProject" `
     -excludebyattribute:*.excludefromcodecoverage* `
@@ -17,6 +17,6 @@ New-Item -Path ..\ -Type Directory -Name $ReportFolderName -Force | Out-Null
     -output:"$ReportFolderPath\results.xml" `
     -searchdirs:"$TestProjectFolderPath\bin\debug\netcoreapp2.0"
 
-&"$NuGetPackages\reportgenerator\4.0.8\tools\net47\ReportGenerator.exe" `
+&"$NuGetPackages\reportgenerator\4.0.14\tools\net47\ReportGenerator.exe" `
     -reports:"$ReportFolderPath\results.xml" `
     -targetdir:"$ReportFolderPath"
